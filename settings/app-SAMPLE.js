bf2298e1e92fb02cec3cf76419b4d27f7ddcb79f
David Halls <dhalls@comecero.com>
2018-10-26 08:00:54 -0600

    Create sample files for settings, remove local copies and update gulp task to create copies for local development

---
+++ settings/app-SAMPLE.js
@@ -0,0 +1,28 @@
+﻿// Make a copy of this file and name it app.js, then place custom values to customize the app's behavior.
+// At runtime, the user's app settings will be automatically injected, providing the user's selections to the app.
+window.__settings = window.__settings || {}; window.__settings.app =
+    {
+    "account_id": "AO0000",
+    "test": true,
+    "development": true,
+    "company_name": "MyCo, Inc.",
+    "error_notifications": "moderate",
+    "show_promo_code": true,
+    "promo_code_name": "Promo Code",
+    "exp_select": false,
+    "ask_save_cards": false,
+    "show_digital_delivery": true,
+    "enable_languages": true,
+    "header_html": null,
+    "footer_html": null,
+    "footer_text": null,
+    "not_found_url": null,
+    "use_product_icon": false,
+    "ask_vat_number": true,
+    "show_card_logos": true,
+    "receipt_close_button_text": "finish",
+    "hide_email_if_provided": false,
+    "hide_name_if_provided": false,
+    "allowed_origin_hosts": "www.example.com,example.com,something.example.com",
+    "main_shopping_url": "http://www.example.com"
+}
---
+++ settings/style-SAMPLE.js
@@ -0,0 +1,18 @@
+﻿// Make a copy of this file and name it style.js, which can be used by the app to customize app styles.
+// Note that the style values provided by the user are compiled into a css stylesheet (located in sass/style.css). This file gives you access to the raw user style input values.
+// At runtime, the user's style settings will be automatically injected, providing the user's selections to the app.
+window.__settings = window.__settings || {}; window.__settings.style =
+    {
+        "company-name-color": "#3a3a3a",
+        "font-family-company-name": "\"Helvetica Neue\", Helvetica, Arial, sans-serif",
+        "font-family-base": "\"Helvetica Neue\", Helvetica, Arial, sans-serif",
+        "header-bg-color": "#d8d8d8",
+        "header-color": "#444444",
+        "body-bg-color": "#ffffff",
+        "body-color": "#444444",
+        "link-color": "#40a5f5",
+        "buynow-button-bg-color": "#2196f3",
+        "buynow-button-color": "#ffffff",
+        "form-error-bg-color": "#fce3e3",
+        "border-radius": "4px"
+    };
