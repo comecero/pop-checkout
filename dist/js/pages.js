4a8011a6c88709691d7e69a21d2ceabbc7797a28
David Halls <dhalls@comecero.com>
2018-11-02 13:44:10 -0600

    Refactor to remove dedicated review page, completion of adding support for Amazon Pay

--- dist/js/pages.js
+++ dist/js/pages.js
@@ -157,7 +157,7 @@ app.controller("IndexController", ['$scope', 'ApiService', 'SettingsService', fu
     window.location = "getting-started";
 
 }]);
-app.controller("CheckoutController", ['$scope', 'CartService', 'GeoService', 'CurrencyService', 'SettingsService', 'HelperService', 'LanguageService', '$uibModal', '$timeout', 'gettextCatalog', '$location', '$document', '$routeParams', function ($scope, CartService, GeoService, CurrencyService, SettingsService, HelperService, LanguageService, $uibModal, $timeout, gettextCatalog, $location, $document, $routeParams) {
+app.controller("CheckoutController", ['$scope', 'CartService', 'GeoService', 'CurrencyService', 'SettingsService', 'HelperService', 'PaymentService', 'LanguageService', 'StorageService', '$uibModal', '$timeout', 'gettextCatalog', '$location', '$document', '$routeParams', function ($scope, CartService, GeoService, CurrencyService, SettingsService, HelperService, PaymentService, LanguageService, StorageService, $uibModal, $timeout, gettextCatalog, $location, $document, $routeParams) {
 
     // Determine if you are running as a modal
     var asModal = $scope.$resolve.asModal;
@@ -166,15 +166,14 @@ app.controller("CheckoutController", ['$scope', 'CartService', 'GeoService', 'Cu
     $scope.data = {};
 
     // Load in the default payment method
-    $scope.options = { "payment_method": "credit_card" };
     $scope.data.showSection = "payment"; // payment, review, receipt
 
     // Load in some helpers
     $scope.geoService = GeoService;
     $scope.settings = SettingsService.get();
     $scope.helpers = HelperService;
-    $scope.options = { showSpinner: false, showForm: false, "payment_method": "credit_card" };
-    $scope.paymentParams = { expand: "payment_method.data,order.customer,order.items.product,order.items.subscription,order.options,cart.options" };
+    $scope.options = { showSpinner: false, showForm: false, payment_method: "credit_card" };
+    $scope.paymentParams = { expand: "payment_method.data,order.customer,order.items.product,order.items.subscription,order.options,cart.options,cart.options" };
 
     // Set the cart parameters
     $scope.data.params = {};
@@ -198,35 +197,68 @@ app.controller("CheckoutController", ['$scope', 'CartService', 'GeoService', 'Cu
         }
     }
 
+    $scope.showSpinner = function (delay) {
+        if (!delay) delay = 0;
+        $scope.options.showForm = false;
+        $scope.spinnerTimeout = $timeout(function () {
+            $scope.options.showSpinner = true;
+        }, delay);
+    }
+
+    $scope.hideSpinner = function () {
+        $scope.options.showForm = true;
+        $scope.options.showSpinner = false;
+        if ($scope.spinnerTimeout)
+            $timeout.cancel($scope.spinnerTimeout);
+    }
+
     // Set the language if supplied by an explicit parameter
     setLanguage($location.search());
 
-    // Get the cart from the query parameters
-    var cart = $location.search().cart;
-    if (cart) {
-        cart = JSON.parse(cart);
-        $location.search("cart", null);
+    // If returning to the page from an external such as PayPal, the payment_id will be in the URL.
+    var payment_id = $routeParams.id;
+
+    if (payment_id) {
+
+        // This is a return from an external page such as PayPal. Get the payment.
+        PaymentService.get(payment_id, $scope.paymentParams).then(function (payment) {
+
+            $scope.data.payment = payment;
+            $scope.data.cart = payment.cart;
+
+            // Check if the payment is already done. The status could be completed (captured) or pending (completed but not yet captured).
+            if (payment.status == "completed" || payment.status == "pending") {
+                $scope.data.showSection = "receipt";
+            } else {
+                $scope.data.showSection = "review";
+            }
+
+            openPop(asModal);
+
+        }, function (error) {
+            $scope.data.error = error;
+            openPop(asModal);
+        });
+
     } else {
-        // Fallback to traditional cart input parameters, useful for manual testing.
-        cart = CartService.fromParams({}, $location);
-    }
 
-    // If iframe modal (desktop), handle targeted messages from the parent window.
-    $scope.$on("messageReceived", function (event, data) {
-        // Examine the message and respond as necessary.
-        if (data.type = "add_to_cart" && data.cart) {
-            showSpinner();
-            setCart(JSON.parse(data.cart));
+        // Get the cart from the query parameters
+        var cart = $location.search().cart;
+        if (cart) {
+            cart = JSON.parse(cart);
+            $location.search("cart", null);
+        } else {
+            // Fallback to traditional cart input parameters, useful for manual testing.
+            cart = CartService.fromParams({}, $location);
         }
-    });
 
-    // If new tab (mobile), run the setCart function on load.
-    if (!asModal) {
-        showSpinner();
-        setCart(cart);
+        // If new tab (mobile), run the setCart function on load.
+        if (!asModal) {
+            $scope.showSpinner(350);
+            setCart(cart);
+        }
     }
 
-    // A function to create the cart
     function setCart(cart) {
 
         // If the customer email has been provided and is invalid, remove it.
@@ -244,10 +276,10 @@ app.controller("CheckoutController", ['$scope', 'CartService', 'GeoService', 'Cu
             $scope.data.cart = cart;
 
             // Hide or cancel showing the spinner
-            hideSpinner();
+            $scope.hideSpinner();
 
             // Show the checkout form
-            showForm(asModal);
+            openPop(asModal);
 
             // Override the header image, as necessary.
             if ($scope.settings.app.use_product_icon && cart.items[0].product.images[0]) {
@@ -277,23 +309,19 @@ app.controller("CheckoutController", ['$scope', 'CartService', 'GeoService', 'Cu
             $scope.data.error = error;
 
             // Hide or cancel showing the spinner
-            hideSpinner();
+            $scope.hideSpinner();
 
             // Open the form
-            showForm(asModal);
+            openPop(asModal);
         });
     }
+    
+    function openPop(asModal) {
 
-    // Show the form, either by modal launch or unhiding if mobile and launched in a new tab.
-    function showForm(asModal) {
-
-        // We load a pageview when the modal opens so that we don't count pageviews for background loads.
-        if (window.__pageview && window.__pageview.recordPageLoad) {
-            window.__pageview.recordPageLoad();
-        }
+        // This is run when the iframe / tab is first launched.
+        $scope.options.showForm = true;
 
         if (asModal) {
-
             // Launch the modal. If you enable ESC support (keyboard: true) you will get a double onClose event unless you refactor how close is handled.
             $scope.modalInstance = $uibModal.open({
                 templateUrl: 'app/pages/simple/checkout.html',
@@ -302,80 +330,87 @@ app.controller("CheckoutController", ['$scope', 'CartService', 'GeoService', 'Cu
                 scope: $scope
             });
 
-        } else {
-            // Show the checkout form
-            $scope.options.showForm = true;
+            function clearModal() {
+                $timeout(function () {
+                    // Reset the payment method. We don't want sensitive payment details to be persisted after the modal is closed.
+                    $scope.selectNewPaymentMethod();
+                    // Clear any errors
+                    $scope.data.error = null;
+                }, 500);
+            }
+
+            $scope.modalInstance.result.then(function () { clearModal() }, function () { clearModal() });
+
+            // We load a pageview when the modal opens so that we don't count pageviews for background loads.
+            if (window.__pageview && window.__pageview.recordPageLoad) {
+                window.__pageview.recordPageLoad();
+            }
         }
     }
 
     // Handle a successful payment
     $scope.onPaymentSuccess = function (payment) {
 
-        $scope.data.order = payment.order;
+        $scope.data.payment = payment;
 
-        // Handle the payment response, depending on the type.
-        switch (payment.payment_method.type) {
+        // Update the cart to the latest
+        $scope.data.cart = payment.cart;
 
-            case "paypal":
+        // If the payment is completed or pending, show the receipt. A payment that is pending is complete, it is just awaiting capture. For the user, there is no difference.
+        if (payment.status == "completed" || payment.status == "pending") {
 
-                // Clear the onbeforeunload event to prevent a "close" event from being sent to the parent.
-                window.onbeforeunload = null;
+            $scope.data.showSection = "receipt";
 
-                // Redirect to PayPal to make the payment.
-                if (asModal) {
-                    sendMessage({ type: "redirect", url: payment.response_data.redirect_url }, $scope.settings.app.allowed_origin_hosts);
-                } else {
-                    window.location = payment.response_data.redirect_url;
-                }
-                break;
+            // Scroll to the top of the page.
+            scrollTop(asModal);
 
-            case "amazon_pay":
-                $scope.data.payment = payment;
-                $scope.data.showSection = "review";
-                break;
+            // Load the conversion
+            if (window.__conversion && window.__conversion.recordConversion) {
+                window.__conversion.recordConversion(payment.order.order_id);
+            }
 
-            default:
-                // Show the receipt.
-                $scope.data.payment = payment;
+        } else {
 
-                // Scroll to top
-                scrollTop(asModal);
+            // Handle follow-on steps, according to the payment method.
+            switch (payment.payment_method.type) {
 
-                // Load the conversion
-                if (window.__conversion && window.__conversion.recordConversion) {
-                    window.__conversion.recordConversion(payment.order.order_id);
-                }
+                case "paypal":
+                    $scope.redirect(payment.response_data.redirect_url);
+                    break;
+
+                case "amazon_pay":
+                    $scope.data.showSection = "review";
+                    break;
+            }
         }
-
     }
 
     $scope.close = function () {
 
-        // Clear any errors
-        $scope.data.error = null;
-
         // Unregister the onbeforeunload event so you don't get a feedback loop.
         window.onbeforeunload = null;
 
-        // Remove any payment info
-        $scope.data.card = { "type": "credit_card" };
-        $scope.data.exp = null;
+        // If scope.data.payment, then they are closing a successful payment. Set the payment to null so any future load will not show the receipt. Reset the order to null.
+        if ($scope.data.payment && ($scope.data.payment.status == "completed" || $scope.data.payment.status == "pending")) {
+            $scope.data.payment = null;
+            StorageService.remove("cart_id");
+        }
 
         // Send a close event to the parent.
         sendMessage({ type: "close", cart: $scope.data.cart, order: $scope.data.order }, $scope.settings.app.allowed_origin_hosts);
-
-        // If scope.data.payment, then they are closing a successful payment. Set the payment to null so any future load will not show the receipt. Reset the order to null.
-        if ($scope.data.payment) {
-            $scope.data.payment = null;
-            $scope.data.order = null;
-        }
 
         // If a modal, close it
         if ($scope.modalInstance) {
             $scope.modalInstance.close();
         } else {
             // Otherwise, close the new tab
-            window.close();
+            if (window.opener != null) {
+                window.close();
+            } else {
+                // We are in a window that we did not open. This can happen when starting in a modal but ending up on the PayPal review page. Redirect to the main shopping URL.
+                var url = localStorage.getItem("parent_url") || SettingsService.get().app.main_shopping_url;
+                window.location.href = url;
+            }
         }
     }
 
@@ -383,9 +418,23 @@ app.controller("CheckoutController", ['$scope', 'CartService', 'GeoService', 'Cu
         $scope.options.payment_method = paymentMethod;
     }
 
-    $scope.selectNewPaymentMethod = function() {
+    $scope.selectNewPaymentMethod = function () {
+
+        delete $scope.data.error;
+        delete $scope.data.amazon_pay.data;
+        $scope.data.card = { "type": "credit_card" };
+        $scope.data.exp = null;
         $scope.options.payment_method = "credit_card";
-        $scope.data.showSection = "payment";
+
+        // If the URL contains the payment_id, we are on the dedicated review page. In that case, we will change the URL to re-start the payment. Otherwise, we will just show the payment section.
+        if (payment_id) {
+            var currentUrl = $location.path().substring(1);
+            currentUrl = "/" + currentUrl.substring(0, currentUrl.indexOf("/"));
+            $location.path(currentUrl);
+        } else {
+            $scope.data.showSection = "payment";
+        }
+
     }
 
     // Handle if the user closes the tab directly.
@@ -400,20 +449,7 @@ app.controller("CheckoutController", ['$scope', 'CartService', 'GeoService', 'Cu
         }
     }
 
-    function showSpinner() {
-        $scope.spinnerTimeout = $timeout(function () {
-            $scope.options.showSpinner = true;
-        }, 350);
-    }
-
-    function hideSpinner() {
-        $scope.options.showSpinner = false;
-        if ($scope.spinnerTimeout)
-            $timeout.cancel($scope.spinnerTimeout);
-    }
-
     function scrollTop(asModal) {
-
         if (asModal) {
             // Scroll to the top of the modal location
             var elem = document.getElementsByClassName("modal");
@@ -424,13 +460,91 @@ app.controller("CheckoutController", ['$scope', 'CartService', 'GeoService', 'Cu
             // Scroll to the top of the document
             $document.scrollTop(0, 500);
         }
+    }
 
+    $scope.redirect = function redirect(url) {
+
+        // Hide the form, show the spinner.
+        $scope.showSpinner();
+
+        // Clear the onbeforeunload event to prevent a "close" event from being sent to the parent during the redirect.
+        window.onbeforeunload = null;
+
+        if (asModal) {
+            sendMessage({ type: "redirect", url: url }, $scope.settings.app.allowed_origin_hosts);
+        } else {
+            window.location = url;
+        }
+
+    }
+
+    $scope.downloadReceipt = function () {
+        ApiService.getItemPdf($scope.data.payment.order.url).then(function (response) {
+
+            var file = new Blob([response.data], { type: "application/pdf" });
+            saveAs(file, "Order_" + $scope.data.payment.order.order_id + ".pdf");
+
+        }, function (error) {
+            $scope.exception.error = error;
+        });
+    }
+
+    $scope.showPaymentSelections = function() {
+        
+        if ($scope.data && $scope.data.amazon_pay && $scope.data.amazon_pay.data)
+            return false;
+
+        if ($scope.settings && $scope.settings.account.payment_method_types.length < 2)
+            return false;
+
+        if ($scope.data && $scope.data.cart && $scope.data.cart.options.payment_methods.length < 2)
+            return false;
+
+        return true;
+    }
+
+    // Select the default payment method
+    $scope.$watch("data.cart.options.payment_methods", function (newVal, oldVal) {
+        if (newVal && newVal != oldVal) {
+            $scope.options.payment_method = getDefaultPaymentMethodType(newVal);
+        }
+    }, true);
+
+    function getDefaultPaymentMethodType(paymentMethods) {
+
+        // Provide options.payment_methods object as the paymentMethods parameter in this function.
+
+        // If you don't have what you are expecting, return null.
+        if (!paymentMethods || paymentMethods.length == 0 || !paymentMethods[0].payment_method_type)
+            return null;
+
+        // Returned in order of priority.
+        if (_.findWhere(paymentMethods, { "payment_method_type": "credit_card" }) != null)
+            return "credit_card";
+
+        if (_.findWhere(paymentMethods, { "payment_method_type": "paypal" }) != null)
+            return "paypal";
+
+        if (_.findWhere(paymentMethods, { "payment_method_type": "amazon_pay" }) != null)
+            return "amazon_pay";
+
+        // Select the first optino as a catch-all
+        return paymentMethods[0].payment_method_type;
     }
 
     // Watch for error to be populated, and if so, scroll to it.
     $scope.$watch("data.error", function (newVal, oldVal) {
         if ($scope.data.error) {
             scrollTop(asModal);
+        }
+    });
+
+    // If iframe modal (desktop), handle targeted messages from the parent window.
+    $scope.$on("messageReceived", function (event, data) {
+        // Examine the message and respond as necessary.
+        if (data.type == "add_to_cart" && data.cart) {
+            $scope.showSpinner(350);
+            setCart(JSON.parse(data.cart));
         }
     });
 
